8e17dcf84d2740b66dd0535f78ef36a03dc1f197
EC2 Default User <ec2-user@ip-172-31-88-139.ec2.internal>
2022-09-26 12:46:05 +0000

    [Add] 会員一覧画面、[Update]新規会員登録画面、ヘッダーのパス設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-
- devise_for :customers,skip:[:passwords], controllers: {
+  devise_for :customers,skip:[:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
@@ -14,6 +13,12 @@ Rails.application.routes.draw do
 namespace :admin do
     resources :genres,only: [:index, :edit, :create, :update]
     resources :items,only: [:index, :new, :show, :edit, :create, :update]
+    resources :customers,only: [:index, :show, :edit, :update]
+  end
+
+ namespace :public do
+    root to: "homes#top"
+    get 'homes/about'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
