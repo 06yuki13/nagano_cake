fc07306cb8339d36b19144ac0053080e19a0f3ca
06yuki13 <06yuki.nakagawa@gmail.com>
2022-10-04 09:54:03 +0000

    [Update]Topページ新着商品

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,8 @@
 class Public::HomesController < ApplicationController
   def top
+   #@item = Item.limit(4).order(" created_at DESC ")
+   reverse_items = Item.order(id:"DESC")
+   @items = reverse_items.first(4)
   end
 
   def about
