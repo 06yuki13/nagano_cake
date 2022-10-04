601aaa0d522ad669ffd9a8d9e3f6a4384b700f4e
06yuki13 <06yuki.nakagawa@gmail.com>
2022-10-04 19:05:33 +0000

    [Add]public/customers/edit,show画面

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,13 +1,27 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+     @customer = Customer.find(params[:id])
+  end
+
+  def update
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
+    redirect_to customer_path(customer.id)
   end
 
   def confirm
   end
 
   def withdrawal
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_active)
   end
 end
