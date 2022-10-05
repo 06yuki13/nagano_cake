a16755ab96212192b3063b772dfc5a61bdb0e310
06yuki13 <06yuki.nakagawa@gmail.com>
2022-10-05 08:37:16 +0000

    [Update]application.htmlヘッダー,after_sign_in/out後の遷移先

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
   if resource == :admin
-    admin_path
+    new_admin_session_path
   else
     root_path
   end
